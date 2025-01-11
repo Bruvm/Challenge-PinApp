ef5ec158f204a3f5d0f2491a77f681d2ef9db30d
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-11 12:39:51 -0300

    cambios

---
+++ api/json-server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import jsonServer from 'json-server';
+import path from 'path';
+
+const app = express();
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const middlewares = jsonServer.defaults();
+
+app.use(middlewares);
+app.use('/api', router); 
+app.listen(3001, () => {
+  console.log('JSON Server is running at http://localhost:3001');
+});
