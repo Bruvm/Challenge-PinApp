38a384af93e72a0e3666ec57a14ac6d815bc0c86
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-11 11:25:56 -0300

    cambios

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, 
   },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://challenge-pin-app.vercel.app//:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
