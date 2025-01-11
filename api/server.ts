6bd0e471085f4e000d35b076aaa656ff0b04a131
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-11 12:01:07 -0300

    cambios

---
+++ api/server.ts
@@ -0,0 +1,26 @@
+// See https://github.com/typicode/json-server#module
+import jsonServer from 'json-server';
+import path from 'path';
+import fs from 'fs';
+
+const server = jsonServer.create();
+
+const router = jsonServer.router('db.json');
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id',
+  })
+);
+
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+export default server;
