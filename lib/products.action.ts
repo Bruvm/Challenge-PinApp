7aff47d3ea3dfc8ccdeb2c96d517d231585b75c1
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-12 18:54:17 -0300

    cambios

--- lib/products.action.ts
+++ lib/products.action.ts
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export async function getSearchProduct(query: string) {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/json-server/products`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
