aabc2161cc321afd5717a3093e1c89ada4ed1f06
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-13 11:22:37 -0300

    test

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -1,5 +1,24 @@
 "use client";
+
+import { getProductBySku } from "@/lib/products.action";
+import { useEffect } from "react";
+
 export default function ProductsPage () {
+    const handleSearch = async () => {
+      try {
+        let response
+          response = await getProductBySku('P001');
+          console.log('response')
+      } catch (error) {
+        console.error("Error", error);
+      }
+    };
+  
+    useEffect(() => {
+
+      handleSearch()
+  
+    }, []);
   return null
 }
 
