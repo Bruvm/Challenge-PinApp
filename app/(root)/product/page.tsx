46f73355717c35d8b31501adc23fd17ec9eaef40
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-13 14:59:42 -0300

    test

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,22 +1,4 @@
 "use client";
-
-import { getProductBySku } from "@/lib/products.action";
-import { useEffect } from "react";
-
 export default function ProductsPage () {
-    const handleSearch = async () => {
-      try {
-        const response = await getProductBySku('P001');
-          console.log('response', response)
-      } catch (error) {
-        console.error("Error", error);
-      }
-    };
-  
-    useEffect(() => {
-
-      handleSearch()
-  
-    }, []);
   return null
 }
