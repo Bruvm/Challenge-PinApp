aa06e81b9d4dfb290e3fde480896971d1816471b
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-01-12 22:42:28 -0300

    d

--- api/server.js
+++ api/server.js
@@ -1,15 +1,19 @@
-const jsonServer = require("json-server");
-const path = require("path");
-const cors = require("cors");
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../db.json"));
-const middlewares = jsonServer.defaults();
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/products/*': '/:sku'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-server.use(cors());
-server.use(middlewares);
-server.use(router);
-
-module.exports = (req, res) => {
-  server(req, res);
-};
+// Export the Server API
+module.exports = server
